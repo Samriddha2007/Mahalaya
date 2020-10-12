fd4e33f045714df3959c3e52bb9c791f7445ed35
Samriddha2007 <67498935+Samriddha2007@users.noreply.github.com>
2020-10-12 19:56:03 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -525,7 +525,7 @@ function keyPressed()
 
 async function getBG()
 {
-    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
     var responseJSON = await response.json();
     var dateTime = responseJSON.datetime;
     var hour = dateTime.slice(11,13);
